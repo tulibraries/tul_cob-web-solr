6b3f3000e84053b4107f315bcce5e99b59eeacae
sensei100 <tuh42082@temple.edu>
2019-10-18 13:57:25 -0400

    fix test.sh file

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
-STATUS=curl http://localhost:8090/solr/tul_cob-web/admin/ping | jq .status
 
-if [ $STATUS != "OK" ]; then
+set -e
+
+STATUS=$(curl http://localhost:8090/solr/tul_cob-web/admin/ping | jq .status)
+
+if [ "$STATUS" != '"OK"' ]; then
   echo "Failing because status is not okay"
-  echo $STATUS
+  echo "Status: $STATUS"
   exit -1
 fi
