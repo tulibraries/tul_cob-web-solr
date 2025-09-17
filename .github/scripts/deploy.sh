694ca5c54994776ff5795318493cb74a722d6c98
Chris Doyle <cdoyle@temple.edu>
2025-09-17 11:12:33 -0400

    update var declarations

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -21,45 +21,43 @@ validate_create() {
   fi
 }
 
-# Use GitHub tag or manual input
-TAG_NAME=${GITHUB_TAG:-$1}
-
-if [ -z "$TAG_NAME" ]; then
-  echo "Error: No tag name provided"
+# RELEASE_TAG should be defined in the workflow environment
+if [ -z "$RELEASE_TAG" ]; then
+  echo "Error: No release tag provided"
   exit 1
 fi
 
 echo
 echo "***"
-echo "* Sending tul_cob-web-$TAG_NAME configs to solrcloud-rocky9."
+echo "* Sending tul_cob-web-$RELEASE_TAG configs to solrcloud-rocky9."
 echo "***"
-RESP=$(curl -u $SOLR_USER:$SOLR_PASSWORD -i -o - --silent -X POST --header "Content-Type:application/octet-stream" --data-binary @$HOME/solrconfig.zip "https://solrcloud-rocky9.tul-infra.page/solr/admin/configs?action=UPLOAD&name=tul_cob-web-$TAG_NAME")
+RESP=$(curl -u $SOLR_USER:$SOLR_PASSWORD -i -o - --silent -X POST --header "Content-Type:application/octet-stream" --data-binary @$HOME/solrconfig.zip "https://solrcloud-rocky9.tul-infra.page/solr/admin/configs?action=UPLOAD&name=tul_cob-web-$RELEASE_TAG")
 validate_status
 
 echo
 echo "***"
-echo "* Creating new tul_cob-web-$TAG_NAME collection"
+echo "* Creating new tul_cob-web-$RELEASE_TAG collection"
 echo "***"
-RESP=$(curl -u $SOLR_USER:$SOLR_PASSWORD -i -o - --silent -X GET --header 'Accept: application/json' "https://solrcloud-rocky9.tul-infra.page/solr/admin/collections?action=CREATE&name=tul_cob-web-$TAG_NAME-init&numShards=1&replicationFactor=4&maxShardsPerNode=1&collection.configName=tul_cob-web-$TAG_NAME")
+RESP=$(curl -u $SOLR_USER:$SOLR_PASSWORD -i -o - --silent -X GET --header 'Accept: application/json' "https://solrcloud-rocky9.tul-infra.page/solr/admin/collections?action=CREATE&name=tul_cob-web-$RELEASE_TAG-init&numShards=1&replicationFactor=4&maxShardsPerNode=1&collection.configName=tul_cob-web-$RELEASE_TAG")
 validate_status
 
 echo
 echo "***"
 echo "* Creating qa alias based on configset name."
 echo "***"
-RESP=$(curl -u $SOLR_USER:$SOLR_PASSWORD -i -o - --silent -X POST --header "Content-Type:application/octet-stream" "https://solrcloud-rocky9.tul-infra.page/solr/admin/collections?action=CREATEALIAS&name=tul_cob-web-$TAG_NAME-qa&collections=tul_cob-web-$TAG_NAME-init")
+RESP=$(curl -u $SOLR_USER:$SOLR_PASSWORD -i -o - --silent -X POST --header "Content-Type:application/octet-stream" "https://solrcloud-rocky9.tul-infra.page/solr/admin/collections?action=CREATEALIAS&name=tul_cob-web-$RELEASE_TAG-qa&collections=tul_cob-web-$RELEASE_TAG-init")
 validate_status
 
 echo
 echo "***"
 echo "* Creating stage alias based on configset name."
 echo "***"
-RESP=$(curl -u $SOLR_USER:$SOLR_PASSWORD -i -o - --silent -X POST --header "Content-Type:application/octet-stream" "https://solrcloud-rocky9.tul-infra.page/solr/admin/collections?action=CREATEALIAS&name=tul_cob-web-$TAG_NAME-stage&collections=tul_cob-web-$TAG_NAME-init")
+RESP=$(curl -u $SOLR_USER:$SOLR_PASSWORD -i -o - --silent -X POST --header "Content-Type:application/octet-stream" "https://solrcloud-rocky9.tul-infra.page/solr/admin/collections?action=CREATEALIAS&name=tul_cob-web-$RELEASE_TAG-stage&collections=tul_cob-web-$RELEASE_TAG-init")
 validate_status
 
 echo
 echo "***"
 echo "* Creating prod alias based on configset name."
 echo "***"
-RESP=$(curl -u $SOLR_USER:$SOLR_PASSWORD -i -o - --silent -X POST --header "Content-Type:application/octet-stream" "https://solrcloud-rocky9.tul-infra.page/solr/admin/collections?action=CREATEALIAS&name=tul_cob-web-$TAG_NAME-prod&collections=tul_cob-web-$TAG_NAME-init")
+RESP=$(curl -u $SOLR_USER:$SOLR_PASSWORD -i -o - --silent -X POST --header "Content-Type:application/octet-stream" "https://solrcloud-rocky9.tul-infra.page/solr/admin/collections?action=CREATEALIAS&name=tul_cob-web-$RELEASE_TAG-prod&collections=tul_cob-web-$RELEASE_TAG-init")
 validate_status
